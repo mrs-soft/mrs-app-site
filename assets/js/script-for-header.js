8abaf23c7e5235ad6e623f5fbe68e4cbc56e05d4
Ekaterina Zvereva <katuha.hip.hop@gmail.com>
2023-04-27 14:44:37 +0300

    Add mobile menu

--- assets/js/script-for-header.js
+++ assets/js/script-for-header.js
@@ -1,11 +1,50 @@
+// Бэкграунд хэдэра
+
 const pageAbout = document.querySelector(".about");
 const pagePrivacy = document.querySelector(".privacy");
 const pageError = document.querySelector(".error");
 
 const header = document.querySelector(".header");
+const footer = document.querySelector(".footer");
 
 if (pageAbout || pagePrivacy || pageError) {
   header.style.backgroundColor = "#fff";
 } else {
   header.style.backgroundColor = "";
 }
+
+// Боковое меню
+
+const burgerBtn = document.getElementById("burger-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+
+//open mobile menu
+burgerBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  mobileMenu.classList.toggle("active");
+  const scrollY = document.documentElement.style.getPropertyValue("--scroll-y");
+  const body = document.body;
+  body.style.position = "fixed";
+  body.style.top = `-${scrollY}`;
+});
+
+//close mobile menu
+document.getElementById("mobile-menu__close").addEventListener("click", (e) => {
+  console.log("dd");
+  e.stopPropagation();
+  if (e.target !== mobileMenu) {
+    mobileMenu.classList.remove("active");
+    const scrollY = document.body.style.top;
+    document.body.style.position = "";
+    document.body.style.top = "";
+    window.scrollTo(0, parseInt(scrollY || "0") * -1);
+  }
+});
+
+window.addEventListener("scroll", () => {
+  document.documentElement.style.setProperty("--scroll-y", `${window.scrollY}px`);
+});
+
+
+
+

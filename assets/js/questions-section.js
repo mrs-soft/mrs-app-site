050513e941b501407e74f0ca52ac6fe50e476821
Ekaterina Zvereva <katuha.hip.hop@gmail.com>
2023-05-02 11:18:31 +0300

    Add question block

---
+++ assets/js/questions-section.js
@@ -0,0 +1,23 @@
+const acc = document.querySelectorAll(".questions__content__accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    const panel = this.nextElementSibling;
+
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+
+      this.querySelector('.open').style.display = "block"
+      this.querySelector('.close').style.display = "none"
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+
+      this.querySelector('.open').style.display = "none"
+      this.querySelector('.close').style.display = "block"
+    }
+  });
+}
